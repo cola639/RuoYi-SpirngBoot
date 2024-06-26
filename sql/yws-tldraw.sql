07b640ce503d0ec17169fc1be83cdafb19c58348
cola639 <337507950@qq.com>
2024-06-27 01:26:49 +0800

    update: tldraw问题

--- sql/yws-tldraw.sql
+++ sql/yws-tldraw.sql
@@ -1,23 +1,35 @@
 -- ----------------------------
--- tldraw    表结构
+-- tldraw    白板表结构
 -- ----------------------------
-drop table if exists tldraw;
+DROP TABLE IF EXISTS tldraw;
 
-create table tldraw
+CREATE TABLE tldraw
 (
-    id          bigint(20) not null auto_increment comment 'ID',
-    user_id     bigint(20) not null auto_increment comment '用户ID',
-    title         varchar(500) default '' comment '标题',
-    roomId    varchar(500) default '' comment '房间ID',
-    userId      varchar(500) default '' comment '用户ID',
-    type         varchar(500) default '' comment '类型 文件夹或白板文件',
-    cover       varchar(500) default '' comment '封面',
-    isOpen      varchar(500) default '' comment '是否公开',
-    remarks     varchar(255) comment '备注',
-    create_by   varchar(64)  default '' comment '创建者',
-    update_by   varchar(64)  default '' comment '更新者',
-    create_time datetime     default current_timestamp comment '创建时间',
-    update_time datetime     default current_timestamp on update current_timestamp comment '更新时间',
-    primary key (id)
-) engine=innodb auto_increment=1 default charset=utf8 collate=utf8_general_ci comment = 'tldraw表';
+    id          bigint(20)   NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    user_id     bigint(20)   NOT NULL COMMENT '创建用户ID',
+    title       varchar(255) NOT NULL COMMENT '标题',
+    room_id     varchar(255) NOT NULL COMMENT '房间ID',
+    members     varchar(255) NOT NULL COMMENT '用户群组',
+    type        varchar(50)  DEFAULT '' COMMENT '类型 (0文件夹 1白板)',
+    cover       varchar(255) DEFAULT '' COMMENT '封面',
+    status      char(1)      default '0' comment '状态（0私有 1公开）',
+    remarks     varchar(255) COMMENT '备注',
+    create_by   varchar(64)  DEFAULT '' COMMENT '创建者',
+    update_by   varchar(64)  DEFAULT '' COMMENT '更新者',
+    create_time datetime     DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_time datetime     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (id),
+    INDEX idx_user_id (user_id),
+    INDEX idx_roomid (room_id)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_general_ci COMMENT = 'tldraw表';
 
+-- 插入模拟数据
+INSERT INTO tldraw (user_id, title, room_id, members, type, cover, status, remarks, create_by, update_by)
+VALUES (1, '项目A白板', 'room001', '1,2,3', '1', '', '0', '这是项目A的白板文件', 'admin', 'admin'),
+       (2, '项目B文件夹', 'room002', '2,3,4', '0', '', '1', '这是项目B的文件夹', 'admin', 'admin'),
+       (3, '项目C白板', 'room003', '1,3,5', '1', '', '0', '这是项目C的白板文件', 'admin', 'admin'),
+       (4, '项目D文件夹', 'room004', '1,2,4', '0', '', '1', '这是项目D的文件夹', 'admin', 'admin'),
+       (5, '项目E白板', 'room005', '2,4,5', '1', '', '0', '这是项目E的白板文件', 'admin', 'admin');
