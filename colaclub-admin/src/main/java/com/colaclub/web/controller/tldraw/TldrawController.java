0d111d5dbfa4e58318d0bd83b1d0c81a7c3d1f7c
cola639 <337507950@qq.com>
2024-06-27 10:25:15 +0800

    update: tldraw增删改查

---
+++ colaclub-admin/src/main/java/com/colaclub/web/controller/tldraw/TldrawController.java
@@ -0,0 +1,85 @@
+package com.colaclub.web.controller.tldraw;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.colaclub.common.annotation.Log;
+import com.colaclub.common.core.controller.BaseController;
+import com.colaclub.common.core.domain.AjaxResult;
+import com.colaclub.common.enums.BusinessType;
+import com.colaclub.tldraw.domain.Tldraw;
+import com.colaclub.tldraw.service.ITldrawService;
+import com.colaclub.common.utils.poi.ExcelUtil;
+import com.colaclub.common.core.page.TableDataInfo;
+
+/**
+ * tldrawController
+ *
+ * @author colaclub
+ * @date 2024-06-27
+ */
+@RestController
+@RequestMapping("/tldraw/tldraw")
+public class TldrawController extends BaseController {
+  @Autowired private ITldrawService tldrawService;
+
+  /** 查询tldraw列表 */
+  @PreAuthorize("@ss.hasPermi('tldraw:tldraw:list')")
+  @GetMapping("/list")
+  public TableDataInfo list(Tldraw tldraw) {
+    startPage();
+    List<Tldraw> list = tldrawService.selectTldrawList(tldraw);
+    return getDataTable(list);
+  }
+
+  /** 导出tldraw列表 */
+  @PreAuthorize("@ss.hasPermi('tldraw:tldraw:export')")
+  @Log(title = "tldraw", businessType = BusinessType.EXPORT)
+  @PostMapping("/export")
+  public void export(HttpServletResponse response, Tldraw tldraw) {
+    List<Tldraw> list = tldrawService.selectTldrawList(tldraw);
+    ExcelUtil<Tldraw> util = new ExcelUtil<Tldraw>(Tldraw.class);
+    util.exportExcel(response, list, "tldraw数据");
+  }
+
+  /** 获取tldraw详细信息 */
+  @PreAuthorize("@ss.hasPermi('tldraw:tldraw:query')")
+  @GetMapping(value = "/{id}")
+  public AjaxResult getInfo(@PathVariable("id") Long id) {
+    return AjaxResult.success(tldrawService.selectTldrawById(id));
+  }
+
+  /** 新增tldraw */
+  @PreAuthorize("@ss.hasPermi('tldraw:tldraw:add')")
+  @Log(title = "tldraw", businessType = BusinessType.INSERT)
+  @PostMapping
+  public AjaxResult add(@RequestBody Tldraw tldraw) {
+    return toAjax(tldrawService.insertTldraw(tldraw));
+  }
+
+  /** 修改tldraw */
+  @PreAuthorize("@ss.hasPermi('tldraw:tldraw:edit')")
+  @Log(title = "tldraw", businessType = BusinessType.UPDATE)
+  @PutMapping
+  public AjaxResult edit(@RequestBody Tldraw tldraw) {
+    return toAjax(tldrawService.updateTldraw(tldraw));
+  }
+
+  /** 删除tldraw */
+  @PreAuthorize("@ss.hasPermi('tldraw:tldraw:remove')")
+  @Log(title = "tldraw", businessType = BusinessType.DELETE)
+  @DeleteMapping("/{ids}")
+  public AjaxResult remove(@PathVariable Long[] ids) {
+    return toAjax(tldrawService.deleteTldrawByIds(ids));
+  }
+}
---
+++ colaclub-system/src/main/java/com/colaclub/tldraw/domain/Tldraw.java
@@ -0,0 +1,142 @@
+package com.colaclub.tldraw.domain;
+
+import com.colaclub.common.annotation.Excel;
+import com.colaclub.common.core.domain.BaseEntity;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * tldraw对象 tldraw
+ *
+ * @author colaclub
+ * @date 2024-06-27
+ */
+public class Tldraw extends BaseEntity {
+  private static final long serialVersionUID = 1L;
+
+  /** ID */
+  private Long id;
+
+  /** 创建用户ID */
+  @Excel(name = "创建用户ID")
+  private Long userId;
+
+  /** 标题 */
+  @Excel(name = "标题")
+  private String title;
+
+  /** 房间ID */
+  @Excel(name = "房间ID")
+  private String roomId;
+
+  /** 用户群组 */
+  @Excel(name = "用户群组")
+  private String members;
+
+  /** 类型 (0文件夹 1白板) */
+  @Excel(name = "类型 (0文件夹 1白板)")
+  private String type;
+
+  /** 封面 */
+  @Excel(name = "封面")
+  private String cover;
+
+  /** 状态（0私有 1公开） */
+  @Excel(name = "状态", readConverterExp = "0=私有,1=公开")
+  private String status;
+
+  /** 备注 */
+  @Excel(name = "备注")
+  private String remarks;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(Long userId) {
+    this.userId = userId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getRoomId() {
+    return roomId;
+  }
+
+  public void setRoomId(String roomId) {
+    this.roomId = roomId;
+  }
+
+  public String getMembers() {
+    return members;
+  }
+
+  public void setMembers(String members) {
+    this.members = members;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getCover() {
+    return cover;
+  }
+
+  public void setCover(String cover) {
+    this.cover = cover;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getRemarks() {
+    return remarks;
+  }
+
+  public void setRemarks(String remarks) {
+    this.remarks = remarks;
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+        .append("id", getId())
+        .append("userId", getUserId())
+        .append("title", getTitle())
+        .append("roomId", getRoomId())
+        .append("members", getMembers())
+        .append("type", getType())
+        .append("cover", getCover())
+        .append("status", getStatus())
+        .append("remarks", getRemarks())
+        .append("createBy", getCreateBy())
+        .append("updateBy", getUpdateBy())
+        .append("createTime", getCreateTime())
+        .append("updateTime", getUpdateTime())
+        .toString();
+  }
+}
