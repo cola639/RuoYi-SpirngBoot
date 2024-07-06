f4dcd374d364528a73248e7f42b838660ce42317
cola639 <337507950@qq.com>
2024-07-07 01:07:18 +0800

    update: 用户头像上传到minio

--- colaclub-admin/src/main/java/com/colaclub/web/controller/system/SysProfileController.java
+++ colaclub-admin/src/main/java/com/colaclub/web/controller/system/SysProfileController.java
@@ -1,7 +1,6 @@
 package com.colaclub.web.controller.system;
 
 import com.colaclub.common.annotation.Log;
-import com.colaclub.common.config.RuoYiConfig;
 import com.colaclub.common.constant.UserConstants;
 import com.colaclub.common.core.controller.BaseController;
 import com.colaclub.common.core.domain.AjaxResult;
@@ -10,8 +9,8 @@ import com.colaclub.common.core.domain.model.LoginUser;
 import com.colaclub.common.enums.BusinessType;
 import com.colaclub.common.utils.SecurityUtils;
 import com.colaclub.common.utils.StringUtils;
-import com.colaclub.common.utils.file.FileUploadUtils;
-import com.colaclub.common.utils.file.MimeTypeUtils;
+import com.colaclub.common.utils.file.MinioOSSUtils;
+import com.colaclub.common.utils.uuid.UUID;
 import com.colaclub.framework.web.service.TokenService;
 import com.colaclub.system.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,98 +25,103 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/system/user/profile")
 public class SysProfileController extends BaseController {
-    @Autowired
-    private ISysUserService userService;
+  @Autowired private ISysUserService userService;
 
-    @Autowired
-    private TokenService tokenService;
+  @Autowired private TokenService tokenService;
+  @Autowired private MinioOSSUtils minioOSSUtils;
 
-    /**
-     * 个人信息
-     */
-    @GetMapping
-    public AjaxResult profile() {
-        LoginUser loginUser = getLoginUser();
-        SysUser user = loginUser.getUser();
-        AjaxResult ajax = AjaxResult.success(user);
-        ajax.put("roleGroup", userService.selectUserRoleGroup(loginUser.getUsername()));
-        ajax.put("postGroup", userService.selectUserPostGroup(loginUser.getUsername()));
+  private static String getFileExtension(String mimeType) {
+    if (mimeType != null && mimeType.contains("/")) {
+      return mimeType.substring(mimeType.lastIndexOf("/") + 1);
+    }
+    return "";
+  }
+
+  /** 个人信息 */
+  @GetMapping
+  public AjaxResult profile() {
+    LoginUser loginUser = getLoginUser();
+    SysUser user = loginUser.getUser();
+    AjaxResult ajax = AjaxResult.success(user);
+    ajax.put("roleGroup", userService.selectUserRoleGroup(loginUser.getUsername()));
+    ajax.put("postGroup", userService.selectUserPostGroup(loginUser.getUsername()));
+    return ajax;
+  }
+
+  /** 修改用户 */
+  @Log(title = "个人信息", businessType = BusinessType.UPDATE)
+  @PutMapping
+  public AjaxResult updateProfile(@RequestBody SysUser user) {
+    LoginUser loginUser = getLoginUser();
+    SysUser sysUser = loginUser.getUser();
+    user.setUserName(sysUser.getUserName());
+    if (StringUtils.isNotEmpty(user.getPhonenumber())
+        && UserConstants.NOT_UNIQUE.equals(userService.checkPhoneUnique(user))) {
+      return AjaxResult.error("修改用户'" + user.getUserName() + "'失败，手机号码已存在");
+    }
+    if (StringUtils.isNotEmpty(user.getEmail())
+        && UserConstants.NOT_UNIQUE.equals(userService.checkEmailUnique(user))) {
+      return AjaxResult.error("修改用户'" + user.getUserName() + "'失败，邮箱账号已存在");
+    }
+    user.setUserId(sysUser.getUserId());
+    user.setPassword(null);
+    if (userService.updateUserProfile(user) > 0) {
+      // 更新缓存用户信息
+      sysUser.setNickName(user.getNickName());
+      sysUser.setPhonenumber(user.getPhonenumber());
+      sysUser.setEmail(user.getEmail());
+      sysUser.setSex(user.getSex());
+      tokenService.setLoginUser(loginUser);
+      return AjaxResult.success();
+    }
+    return AjaxResult.error("修改个人信息异常，请联系管理员");
+  }
+
+  /** 重置密码 */
+  @Log(title = "个人信息", businessType = BusinessType.UPDATE)
+  @PutMapping("/updatePwd")
+  public AjaxResult updatePwd(String oldPassword, String newPassword) {
+    LoginUser loginUser = getLoginUser();
+    String userName = loginUser.getUsername();
+    String password = loginUser.getPassword();
+    if (!SecurityUtils.matchesPassword(oldPassword, password)) {
+      return AjaxResult.error("修改密码失败，旧密码错误");
+    }
+    if (SecurityUtils.matchesPassword(newPassword, password)) {
+      return AjaxResult.error("新密码不能与旧密码相同");
+    }
+    if (userService.resetUserPwd(userName, SecurityUtils.encryptPassword(newPassword)) > 0) {
+      // 更新缓存用户密码
+      loginUser.getUser().setPassword(SecurityUtils.encryptPassword(newPassword));
+      tokenService.setLoginUser(loginUser);
+      return AjaxResult.success();
+    }
+    return AjaxResult.error("修改密码异常，请联系管理员");
+  }
+
+  /** 头像上传 */
+  @Log(title = "用户头像", businessType = BusinessType.UPDATE)
+  @PostMapping("/avatar")
+  public AjaxResult avatar(@RequestParam("avatarfile") MultipartFile file) throws Exception {
+    if (!file.isEmpty()) {
+      String userName = getUsername();
+      String uuid = UUID.randomUUID().toString();
+      String mimeType = file.getContentType();
+      String fileExtension = getFileExtension(mimeType);
+      // 构造最终文件名
+      String finalFileName = "images/" + uuid + "_" + userName + "." + fileExtension;
+      // 使用 MinioOSSUtils 上传文件
+      String avatar = minioOSSUtils.putObject(finalFileName, file);
+      LoginUser loginUser = getLoginUser();
+      if (userService.updateUserAvatar(loginUser.getUsername(), avatar)) {
+        AjaxResult ajax = AjaxResult.success();
+        ajax.put("imgUrl", avatar);
+        // 更新缓存用户头像
+        loginUser.getUser().setAvatar(avatar);
+        tokenService.setLoginUser(loginUser);
         return ajax;
+      }
     }
-
-    /**
-     * 修改用户
-     */
-    @Log(title = "个人信息", businessType = BusinessType.UPDATE)
-    @PutMapping
-    public AjaxResult updateProfile(@RequestBody SysUser user) {
-        LoginUser loginUser = getLoginUser();
-        SysUser sysUser = loginUser.getUser();
-        user.setUserName(sysUser.getUserName());
-        if (StringUtils.isNotEmpty(user.getPhonenumber())
-                && UserConstants.NOT_UNIQUE.equals(userService.checkPhoneUnique(user))) {
-            return AjaxResult.error("修改用户'" + user.getUserName() + "'失败，手机号码已存在");
-        }
-        if (StringUtils.isNotEmpty(user.getEmail())
-                && UserConstants.NOT_UNIQUE.equals(userService.checkEmailUnique(user))) {
-            return AjaxResult.error("修改用户'" + user.getUserName() + "'失败，邮箱账号已存在");
-        }
-        user.setUserId(sysUser.getUserId());
-        user.setPassword(null);
-        if (userService.updateUserProfile(user) > 0) {
-            // 更新缓存用户信息
-            sysUser.setNickName(user.getNickName());
-            sysUser.setPhonenumber(user.getPhonenumber());
-            sysUser.setEmail(user.getEmail());
-            sysUser.setSex(user.getSex());
-            tokenService.setLoginUser(loginUser);
-            return AjaxResult.success();
-        }
-        return AjaxResult.error("修改个人信息异常，请联系管理员");
-    }
-
-    /**
-     * 重置密码
-     */
-    @Log(title = "个人信息", businessType = BusinessType.UPDATE)
-    @PutMapping("/updatePwd")
-    public AjaxResult updatePwd(String oldPassword, String newPassword) {
-        LoginUser loginUser = getLoginUser();
-        String userName = loginUser.getUsername();
-        String password = loginUser.getPassword();
-        if (!SecurityUtils.matchesPassword(oldPassword, password)) {
-            return AjaxResult.error("修改密码失败，旧密码错误");
-        }
-        if (SecurityUtils.matchesPassword(newPassword, password)) {
-            return AjaxResult.error("新密码不能与旧密码相同");
-        }
-        if (userService.resetUserPwd(userName, SecurityUtils.encryptPassword(newPassword)) > 0) {
-            // 更新缓存用户密码
-            loginUser.getUser().setPassword(SecurityUtils.encryptPassword(newPassword));
-            tokenService.setLoginUser(loginUser);
-            return AjaxResult.success();
-        }
-        return AjaxResult.error("修改密码异常，请联系管理员");
-    }
-
-    /**
-     * 头像上传
-     */
-    @Log(title = "用户头像", businessType = BusinessType.UPDATE)
-    @PostMapping("/avatar")
-    public AjaxResult avatar(@RequestParam("avatarfile") MultipartFile file) throws Exception {
-        if (!file.isEmpty()) {
-            LoginUser loginUser = getLoginUser();
-            String avatar = FileUploadUtils.upload(RuoYiConfig.getAvatarPath(), file, MimeTypeUtils.IMAGE_EXTENSION);
-            if (userService.updateUserAvatar(loginUser.getUsername(), avatar)) {
-                AjaxResult ajax = AjaxResult.success();
-                ajax.put("imgUrl", avatar);
-                // 更新缓存用户头像
-                loginUser.getUser().setAvatar(avatar);
-                tokenService.setLoginUser(loginUser);
-                return ajax;
-            }
-        }
-        return AjaxResult.error("上传图片异常，请联系管理员");
-    }
+    return AjaxResult.error("上传图片异常，请联系管理员");
+  }
 }
